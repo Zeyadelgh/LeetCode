4c2c288c081d991d1b3ebc2bc80417f84913f8d7
Zeyadelgh <49723078+Zeyadelgh@users.noreply.github.com>
2022-07-24 19:20:41 -0400

    Added notes

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -1,6 +1,22 @@
 /*
- * Runtime: 11 ms, faster than 68.00% of Java online submissions for Contains Duplicate.
- * Memory Usage: 69.9 MB, less than 43.87% of Java online submissions for Contains Duplicate.
+ * Challenge:
+ * 		Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ * 
+ * Constraints:
+ * 		1 <= nums.length <= 10^5
+ *		-10^9 <= nums[i] <= 10^9
+ *
+ * Runtime:
+ * 		 11 ms, faster than 68.00% of Java online submissions for Contains Duplicate.
+ * 		 Memory Usage: 69.9 MB, less than 43.87% of Java online submissions for Contains Duplicate.
+ * 
+ * I decided to use a HashMap because of the contstraint 1 <= nums.length <= 10^5. Doing a traditional loop would take too long.
+ * In the HashMap, the put method returns null if the key is unique or new, assuming there is no null keys else it returns the previous key.
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * 
+ * 
  */
 
 import java.util.HashMap;
@@ -14,13 +30,13 @@ public class ContainsDuplicate {
 	}
 
 	public static boolean containsDuplicate(int[] nums) {
-		
-		Map<Integer, Integer> ht1= new HashMap<Integer, Integer>();
+
+		Map<Integer, Integer> ht1 = new HashMap<Integer, Integer>();
 		Integer check = null;
 
-		for (int i = 0; i < nums.length; i++ ) {
-			check = ht1.put(nums[i], i);
-			if (check != null) {
+		for (int i = 0; i < nums.length; i++) {
+			ht1.put(nums[i], i);
+			if (ht1.containsKey(nums[i])) {
 				return true;
 			}
 		}
